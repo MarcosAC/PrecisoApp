68139421958e2534aafc419ed36fbafea364cd52
Marcos Aurelio <marcos.aucorrea@gmail.com>
2021-03-25 12:04:19 -0300

    Corrido erro splashscreen não fechava

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+import React, { useEffect } from 'react';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import SplashScreen from  "react-native-splash-screen";
+
+const Apps = () => {
+    useEffect(() => {
+        SplashScreen.hide();
+    })
+
+    return <App />
+}
+
+AppRegistry.registerComponent(appName, () => Apps);
